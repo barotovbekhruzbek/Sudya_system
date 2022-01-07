1bfa8a85f10fb0814035d06bfa91fef6c0af2756
Barotov Bekhruzbek <95738145+barotovbekhruzbek@users.noreply.github.com>
2022-01-07 15:38:22 +0500

    first commit

---
+++ html5countrymapv4.1/mapdata.js
@@ -0,0 +1,163 @@
+var simplemaps_countrymap_mapdata={
+  main_settings: {
+   //General settings
+    width: "300", //'700' or 'responsive'
+    background_color: "#FFFFFF",
+    background_transparent: "yes",
+    border_color: "#ffffff",
+    
+    //State defaults
+    state_description: "State description",
+    state_color: "#88A4BC",
+    state_hover_color: "#3B729F",
+    state_url: "",
+    border_size: 1.5,
+    all_states_inactive: "no",
+    all_states_zoomable: "yes",
+    
+    //Location defaults
+    location_description: "Location description",
+    location_url: "",
+    location_color: "#FF0067",
+    location_opacity: 0.8,
+    location_hover_opacity: 1,
+    location_size: 25,
+    location_type: "square",
+    location_image_source: "frog.png",
+    location_border_color: "#FFFFFF",
+    location_border: 2,
+    location_hover_border: 2.5,
+    all_locations_inactive: "no",
+    all_locations_hidden: "no",
+    
+    //Label defaults
+    label_color: "#d5ddec",
+    label_hover_color: "#d5ddec",
+    label_size: 22,
+    label_font: "Arial",
+    hide_labels: "no",
+    hide_eastern_labels: "no",
+   
+    //Zoom settings
+    zoom: "yes",
+    manual_zoom: "yes",
+    back_image: "no",
+    initial_back: "no",
+    initial_zoom: "-1",
+    initial_zoom_solo: "no",
+    region_opacity: 1,
+    region_hover_opacity: 0.6,
+    zoom_out_incrementally: "yes",
+    zoom_percentage: 0.99,
+    zoom_time: 0.5,
+    
+    //Popup settings
+    popup_color: "white",
+    popup_opacity: 0.9,
+    popup_shadow: 1,
+    popup_corners: 5,
+    popup_font: "12px/1.5 Verdana, Arial, Helvetica, sans-serif",
+    popup_nocss: "no",
+    
+    //Advanced settings
+    div: "map",
+    auto_load: "yes",
+    url_new_tab: "no",
+    images_directory: "default",
+    fade_time: 0.1,
+    link_text: "View Website",
+    popups: "detect",
+    state_image_url: "",
+    state_image_position: "",
+    location_image_url: ""
+  },
+  state_specific: {
+    UZB354: {
+      name: "Bukhoro",
+      hover_color: "#1D37B5",
+      description: "Buxoro"
+    },
+    UZB355: {
+      name: "Khorezm",
+      hover_color: "#1D37B5",
+      description: "Xorazm"
+    },
+    UZB356: {
+      name: "Karakalpakstan",
+      hover_color: "#1D37B5",
+      description: "Qoraqalpoqiston"
+    },
+    UZB357: {
+      name: "Navoi",
+      hover_color: "#1D37B5",
+      description: "Navoi"
+    },
+    UZB358: {
+      name: "Samarkand",
+      hover_color: "#1D37B5",
+      description: "Samarqand"
+    },
+    UZB361: {
+      name: "Kashkadarya",
+      hover_color: "#1D37B5",
+      description: "Qashqadaryo"
+    },
+    UZB362: {
+      name: "Surkhandarya",
+      hover_color: "#1D37B5",
+      description: "Surxandaryo"
+    },
+    UZB363: {
+      name: "Andijon",
+      hover_color: "#1D37B5",
+      description: "Andijon"
+    },
+    UZB364: {
+      name: "Ferghana",
+      hover_color: "#1D37B5",
+      description: "Fargo`na"
+    },
+    UZB365: {
+      name: "Namangan",
+      hover_color: "#1D37B5",
+      description: "Namangan"
+    },
+    UZB370: {
+      name: "Jizzakh",
+      hover_color: "#1D37B5",
+      description: "Jizzax"
+    },
+    UZB371: {
+      name: "Sirdaryo",
+      hover_color: "#1D37B5",
+      description: "Sirdaryo"
+    },
+    UZB372: {
+      name: "Tashkent",
+      hover_color: "#1D37B5",
+      description: "Toshkent"
+    },
+    UZB4828: {
+      name: "Tashkent",
+      hover_color: "#1D37B5",
+      description: "Toshkent"
+    }
+  },
+  locations: {
+    "0": {
+      lat: "41.316667",
+      lng: "69.25",
+      name: "Tashkent"
+    },
+    "1": {
+      lat: 38.57,
+      lng: 68.788,
+      name: "Bukhoro"
+    }
+  },
+  labels: {},
+  legend: {
+    entries: []
+  },
+  regions: {}
+};
